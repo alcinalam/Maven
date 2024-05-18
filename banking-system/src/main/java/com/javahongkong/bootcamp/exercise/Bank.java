2f0c12d1ef75e3d6f5232c0b72ffbc834497df31
Alcinalam <alcinalam@gmail.com>
2024-05-18 20:43:07 +0800

    update

---
+++ banking-system/src/main/java/com/javahongkong/bootcamp/exercise/Bank.java
@@ -0,0 +1,70 @@
+package com.javahongkong.bootcamp.exercise;
+
+import java.util.LinkedHashMap;
+
+public class Bank implements BankInterface{
+	private LinkedHashMap<Long, Account> accounts; // object reference
+  private static long counter = 0;
+	private final Object lock = new Object();
+
+	public Bank() {
+		// complete the function
+		this.accounts = new LinkedHashMap<>();
+	}
+
+	public Account getAccount(Long accountNumber) {
+		// complete the function
+		return this.accounts.get(accountNumber);
+	}
+	
+	private static long Counter(){
+		return counter++;
+		
+	}
+
+	public Long openCommercialAccount(Company company, int pin, double startingDeposit) {
+		//complete the function
+		
+		synchronized (lock) {
+      Long accountNumber = Bank.Counter();
+    
+		
+		Account newCommercialAcc = new CommercialAccount(company, accountNumber, pin, startingDeposit);
+		this.accounts.put(accountNumber,newCommercialAcc);
+		return accountNumber;
+		}
+	}
+
+	public Long openConsumerAccount(Person person, int pin, double startingDeposit) {
+		//complete the function
+		synchronized (lock) {
+      Long accountNumber = Bank.Counter();
+    
+		
+		Account newConsumerAcc = new ConsumerAccount(person, accountNumber, pin, startingDeposit);
+		this.accounts.put(accountNumber,newConsumerAcc);
+		return accountNumber;
+		}
+	}
+
+	public boolean authenticateUser(Long accountNumber, int pin) {
+		// complete the function
+		return this.getAccount(accountNumber).validatePin(pin);
+	}
+
+	public double getBalance(Long accountNumber) {
+		// complete the function
+		return this.getAccount(accountNumber).getBalance();
+	}
+
+	public void credit(Long accountNumber, double amount) {
+		// complete the function
+		getAccount(accountNumber).creditAccount(amount);
+	}
+
+	public boolean debit(Long accountNumber, double amount) {
+		// complete the function
+		return this.getAccount(accountNumber).debitAccount(amount);
+	}
+
+}
---
+++ banking-system/src/main/java/com/javahongkong/bootcamp/exercise/Transaction.java
@@ -0,0 +1,44 @@
+package com.javahongkong.bootcamp.exercise;
+
+public class Transaction implements TransactionInterface{
+	private Long accountNumber;
+	private Bank bank;
+
+	/**
+	 *
+	 * @param bank
+	 *                      The bank where the account is housed.
+	 * @param accountNumber
+	 *                      The customer's account number.
+	 * @param attemptedPin
+	 *                      The PIN entered by the customer.
+	 * @throws Exception
+	 *                   Account validation failed.
+	 */
+	public Transaction(Bank bank, Long accountNumber, int attemptedPin) throws Exception {
+		// complete the function
+		if (bank.authenticateUser(accountNumber, attemptedPin)==true){
+			this.bank = bank;
+			this.accountNumber = accountNumber;
+		} else {
+			throw new SecurityException("pin not match");
+		}
+
+	}
+
+	public double getBalance() {
+		// complete the function
+		return this.bank.getBalance(this.accountNumber);
+		
+	}
+
+	public void credit(double amount) {
+		// complete the function
+		this.bank.credit(this.accountNumber, amount);
+	}
+
+	public boolean debit(double amount) {
+		// complete the function
+		return this.bank.debit(this.accountNumber, amount);
+	}
+}
